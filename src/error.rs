ebbab4070fa289ef442cdf858aa9ca0f7177fe91
Thorsten Frank <thorsten@tfsw.de>
2022-06-27 14:57:06 -0700

    adds malformed packet error

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,13 @@
 use std::fmt::{self, Display};
 
+/// 
 #[derive(Debug, Clone, PartialEq)]
 pub enum MqttError {
+    
+    /// 
+    MalformedPacket(String),
+
+    /// a general-use error string
     Message(String),
 }
 
@@ -10,6 +16,7 @@ impl std::error::Error for MqttError {}
 impl Display for MqttError {
     fn fmt(&self, formatter: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
+            MqttError::MalformedPacket(detail) => formatter.write_fmt(format_args!("Malformed Packet: {}", detail)),
             MqttError::Message(msg) => formatter.write_str(msg),
             //_ => formatter.write_str("general error"),
         }
